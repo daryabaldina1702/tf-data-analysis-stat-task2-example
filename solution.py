7faec87414f59f62b90889fa7c7333c3ed08757c
daryabaldina1702 <71131519+daryabaldina1702@users.noreply.github.com>
2023-04-06 23:50:18 +0500

    Third solution for task 2

--- solution.py
+++ solution.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
 from scipy.stats import t
 
 chat_id = 834411281 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-   n = len(x)
-   time = 86
-   mean_distance = np.mean(x)
-   variance = 0.5
-   sum_sq_deviation = np.sum((x - mean_distance) ** 2)
-   variance_acceleration = (2 * variance * sum_sq_deviation) / ((n - 1) * time ** 2)
-   std_error_acceleration = np.sqrt(variance_acceleration)
-   t_stat = norm.ppf((1 + p) / 2, n - 1)
-   lower_bound = mean_distance - t_stat * std_error_acceleration
-   upper_bound = mean_distance + t_stat * std_error_acceleration
-   return (lower_bound, upper_bound)
+    n = len(x)
+    time = 86
+    alpha = 1 - p
+    a = np.abs(2 * x / time**2 + 0.5 - np.random.exponential(1, n))
+    mean = np.mean(a) #среднее выборки
+    s = np.std(a, ddof=1)
+    SE = s / np.sqrt(n)
+    t_value = t.ppf(1 - alpha/ 2, n - 1)
+    lower_bound = mean - t_value * SE
+    upper_bound = mean + t_value * SE
+    return (lower_bound, upper_bound)
