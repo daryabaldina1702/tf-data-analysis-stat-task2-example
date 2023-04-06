ec89f370d9ee478c0fc924c020a95e90c6edd1b9
daryabaldina1702 <71131519+daryabaldina1702@users.noreply.github.com>
2023-04-07 00:37:09 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,18 @@
 import numpy as np
-from scipy.stats import gamma
+from scipy.stats import t
 
 chat_id = 834411281
 
 def solution(p: float, x: np.array) -> tuple:
-    left = 2 / 86 ** 2 * (gamma.ppf((1 - p) / 2, x.size) / x.size + x.mean() - 1 / 2)
-    right = 2 / 86 ** 2 * (gamma.ppf((1 + p) / 2, x.size) / x.size + x.mean() - 1 / 2)
-    return left, right
+    n = len(x)
+    time = 86
+    alpha = 1 - p
+    x_norm  = x + 0.5 - np.random.exponential(1, n)
+    a = 2 * x_norm/ time**2
+    mean = np.mean(a) #среднее выборки
+    s = np.std(a, ddof=1)
+    SE = s / np.sqrt(n)
+    t_value = t.ppf(1 - alpha/ 2, n - 1)
+    lower_bound = mean - t_value * SE
+    upper_bound = mean + t_value * SE
+    return (lower_bound, upper_bound)
