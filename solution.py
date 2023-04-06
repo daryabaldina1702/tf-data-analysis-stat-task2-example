9a9a09a20e554d8bb9c12003962a9fd0cb1e0ddc
daryabaldina1702 <71131519+daryabaldina1702@users.noreply.github.com>
2023-04-07 00:07:35 +0500

    Forth solution for task2

--- solution.py
+++ solution.py
@@ -1,18 +1,9 @@
-import pandas as pd
 import numpy as np
-from scipy.stats import t
+from scipy.stats import gamma
 
-chat_id = 834411281 # Ваш chat ID, не меняйте название переменной
+chat_id = 834411281
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    time = 86
-    alpha = 1 - p
-    a = np.abs(2 * x / time**2 + 0.5 - np.random.exponential(1, n))
-    mean = np.mean(a) #среднее выборки
-    s = np.std(a, ddof=1)
-    SE = s / np.sqrt(n)
-    t_value = t.ppf(1 - alpha/ 2, n - 1)
-    lower_bound = mean - t_value * SE
-    upper_bound = mean + t_value * SE
-    return (lower_bound, upper_bound)
+    left = 2 / 86 ** 2 * (gamma.ppf((1 - p) / 2, x.size) / x.size + x.mean() - 1 / 2)
+    right = 2 / 86 ** 2 * (gamma.ppf((1 + p) / 2, x.size) / x.size + x.mean() - 1 / 2)
+    return left, right
